ad3bf7efedb98d11918f3d496bfe9013720934a9
Crowdbotics <team@crowdbotics.com>
2022-11-28 01:07:22 +0000

    Initial commit.

---
+++ backend/somtest_37723/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for somtest_37723 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'somtest_37723.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "somtest_37723",
+  url: "https://somtest_37723.botics.co",
+  api: "https://somtest_37723.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
